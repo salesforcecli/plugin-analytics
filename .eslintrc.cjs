402e9808910a3877611d6503291800161965ad7d
Gregory Smith <gregorysmith@salesforce.com>
2024-01-09 16:24:53 -0700

    Update all the versions

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,7 +5,8 @@
  * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
 module.exports = {
-  extends: ['eslint-config-salesforce-typescript', 'eslint-config-salesforce-license'],
+  extends: ['eslint-config-salesforce-typescript', 'eslint-config-salesforce-license', 'plugin:sf-plugin/recommended'],
+  root: true,
   rules: {
     // this allows you to indent the 2nd line of an @param, which I think helps readability
     'jsdoc/check-indentation': ['error' | 'warn', { excludeTags: ['example', 'param'] }],
